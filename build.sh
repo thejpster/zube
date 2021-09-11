b2ad604d89f9b364f8510aeea516d1d69eb13867
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-09-11 19:48:50 +0100

    Start FPGA build file.

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#
+# Builds zero2asic for a Terasic DE10-Lite
+#
+# Copyright (c) 2021, Jonathan 'theJPster' Pallant
+#
+# Licence: Apache-2.0
+#
+
+set -euo pipefail
+
+PROJECT=zero2asic
+
+mkdir -p fpga_build
+
+yosys -l fpga_build/yosys.log -p "read -sv ./src/${PROJECT}.v; synth_intel -family max10 -top ${PROJECT} -vqm fpga_build/${PROJECT}.vqm -vpr fpga_build/${PROJECT}.vpr; stat" src/*.v
+# TODO: I think we need Quartus here...
