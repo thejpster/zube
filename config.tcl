7b51a1b19fb108ca8764144665cf6383a60fe997
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2021-09-23 21:15:34 +0100

    Harden step now works again.

--- config.tcl
+++ config.tcl
@@ -4,12 +4,14 @@ set ::env(DESIGN_NAME) zube
 # Change if needed
 set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 
-# turn off clock
-set ::env(CLOCK_TREE_SYNTH) 0
-set ::env(CLOCK_PORT) ""
+set ::env(PL_TARGET_DENSITY) 0.60
+# set ::env(FP_CORE_UTIL) 40
 
-# adjust target density (as our design is so smol)
-set ::env(PL_TARGET_DENSITY) 0.75
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 200 200"
+
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
 
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
